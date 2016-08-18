048a993cac09d0add745d9891ab66356e692b1e6
Roger7410 <roger9217605@gmail.com>
2016-08-18 12:10:29 -0700

    Aug 18

--- Solution/src/Test.java
+++ Solution/src/Test.java
@@ -11,14 +11,19 @@ public class Test {
         System.out.println(Integer.MAX_VALUE);
         System.out.println((int) Math.pow(4, (int) (Math.log(Integer.MAX_VALUE) / Math.log(4.0))));
         */
-        String s = "abc";
-        System.out.println(s.substring(1, 2));
-        System.out.println(s.substring(1));
-        HashMap<String,Integer> days = new HashMap<>();
-        int[] ii = new int[0];
-        for(int i=0; i<ii.length;i++){
-            System.out.println("werid");
-        }
+//        String s = "abc";
+//        System.out.println(s.substring(1, 2));
+//        System.out.println(s.substring(1));
+//        HashMap<String,Integer> days = new HashMap<>();
+//        int[] ii = new int[0];
+//        for(int i=0; i<ii.length;i++){
+//            System.out.println("werid");
+//        }
+        String s = "abcd";
+        s = s.substring(1,4);
+        System.out.println(s);
+        char tmp;
+        tmp = s.charAt(0);
 
     }
 }
